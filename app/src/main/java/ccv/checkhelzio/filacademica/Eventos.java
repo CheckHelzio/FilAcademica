a66cb056dcd2174265fec2d30e96b27af043f867
CheckHelzio <checkhelzio@gmail.com>
2016-10-19 18:19:15 -0500

    Add. clase Eventos y Sedes

---
+++ app/src/main/java/ccv/checkhelzio/filacademica/Eventos.java
@@ -0,0 +1,14 @@
+package ccv.checkhelzio.filacademica;
+
+import java.util.List;
+
+/**
+ * Created by check on 19/10/2016.
+ */
+
+public class Eventos {
+    int id_cartel;
+    String titulo;
+    String descripcion;
+
+}
---
+++ app/src/main/java/ccv/checkhelzio/filacademica/Sedes.java
@@ -0,0 +1,51 @@
+package ccv.checkhelzio.filacademica;
+
+/**
+ * Created by check on 19/10/2016.
+ */
+
+public class Sedes {
+    String nombre;
+    String direccion;
+    float longitud;
+    float latitud;
+
+    public Sedes(String nombre, String direccion, float longitud, float latitud) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.longitud = longitud;
+        this.latitud = latitud;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public float getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(float longitud) {
+        this.longitud = longitud;
+    }
+
+    public float getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(float latitud) {
+        this.latitud = latitud;
+    }
+}
