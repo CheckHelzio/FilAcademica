b41c6624099b34191b95e0cb25f287aec9512f72
CheckHelzio <checkhelzio@gmail.com>
2016-10-19 19:29:41 -0500

    Add. portada y menu

---
+++ app/src/main/java/ccv/checkhelzio/filacademica/Menu.java
@@ -0,0 +1,17 @@
+package ccv.checkhelzio.filacademica;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by check on 19/10/2016.
+ */
+
+public class Menu extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_layout);
+    }
+}
--- app/src/main/java/ccv/checkhelzio/filacademica/Portada.java
+++ app/src/main/java/ccv/checkhelzio/filacademica/Portada.java
@@ -1,21 +1,24 @@
 package ccv.checkhelzio.filacademica;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Portada extends AppCompatActivity {
-    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portada);
-        handler = new Handler();
-        handler.postDelayed(new Runnable() {
+
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
+                Intent i = new Intent(Portada.this, Menu.class);
+                startActivity(i);
+                finish();
             }
         },2000);
     }
