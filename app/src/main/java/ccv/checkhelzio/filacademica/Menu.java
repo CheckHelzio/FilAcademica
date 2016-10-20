d42fc7b8988ea9af77ca5651c429ccfea4d42f22
CheckHelzio <checkhelzio@gmail.com>
2016-10-20 15:37:51 -0500

    Add. portada y menu

--- app/src/main/java/ccv/checkhelzio/filacademica/Menu.java
+++ app/src/main/java/ccv/checkhelzio/filacademica/Menu.java
@@ -1,17 +1,34 @@
 package ccv.checkhelzio.filacademica;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by check on 19/10/2016.
  */
 
-public class Menu extends AppCompatActivity{
+public class Menu extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu_layout);
+        FullScreencall();
+        setContentView(R.layout.menu);
+
+    }
+
+    public void FullScreencall() {
+        if(Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            View v = this.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if(Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
     }
 }
