c4c6554d535cb7d618f2afb771499b51f7420efb
CheckHelzio <checkhelzio@gmail.com>
2016-10-25 16:32:50 -0500

    - Nuevo indicador

--- app/src/main/java/ccv/checkhelzio/filacademica/InfoEventoDialog.java
+++ app/src/main/java/ccv/checkhelzio/filacademica/InfoEventoDialog.java
@@ -15,6 +15,7 @@ public class InfoEventoDialog extends Activity {
 
     @BindView(R.id.tv_titulo_evento) TextView tv_titulo_evento;
     @BindView(R.id.tv_subtitulo) TextView tv_subtitulo_evento;
+    @BindView(R.id.tv_descripcion) TextViewEx tv_descripcion;
     Eventos evento;
 
     @Override
@@ -44,12 +45,31 @@ public class InfoEventoDialog extends Activity {
     private void setDatos() {
         tv_titulo_evento.setText(evento.getTitulo());
         tv_subtitulo_evento.setText(evento.getSubtitulo());
+        tv_descripcion.setText(evento.getDescripcion(), true);
     }
 
     @Override
     public void onBackPressed() {
         setResult(RESULT_CANCELED);
         cerrar(null);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FullScreencall();
+    }
+
+    public void FullScreencall() {
+        if(Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            View v = this.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if(Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
     }
 
     public void cerrar(View view) {
